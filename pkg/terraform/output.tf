bd0b63b0d48214b373f8d19eb28e8246fe7966b6
aidocons <consultant-butlyai@aidodev.com>
2025-05-26 12:30:06 +0100

    turn repo format output to lowercase)

--- pkg/terraform/output.tf
+++ pkg/terraform/output.tf
@@ -1,24 +1,24 @@
-output "artifact_registry_repository_id" {
+output "repository_id" {
   description = "The ID of the Artifact Registry repository"
   value       = google_artifact_registry_repository.repo.id
 }
 
-output "artifact_registry_repository_name" {
+output "repository_name" {
   description = "The name of the Artifact Registry repository"
   value       = google_artifact_registry_repository.repo.name
 }
 
-output "artifact_registry_repository_location" {
+output "repository_location" {
   description = "The location of the Artifact Registry repository"
   value       = google_artifact_registry_repository.repo.location
 }
 
-output "artifact_registry_repository_format" {
+output "repository_format" {
   description = "The format of the Artifact Registry repository (e.g., DOCKER, MAVEN, etc.)"
   value       = google_artifact_registry_repository.repo.format
 }
 
-output "artifact_registry_repository_url" {
+output "repository_url" {
   description = "The full URL of the Artifact Registry repository"
-  value       = "https://${google_artifact_registry_repository.repo.location}-docker.pkg.dev/${google_artifact_registry_repository.repo.project}/${google_artifact_registry_repository.repo.name}"
+  value       = "${google_artifact_registry_repository.repo.location}-${lower(google_artifact_registry_repository.repo.format)}.pkg.dev/${google_artifact_registry_repository.repo.project}/${google_artifact_registry_repository.repo.name}"
 }
